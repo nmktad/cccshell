fe7317a8ed7c8bf5da3cc71893ea84e61779cdb8
nathnael.mekonnen <nathnael.mekonnen@a2sv.org>
2025-11-15 14:32:20 +0100

    feat: handle invalid commands

--- src/main.c
+++ src/main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BUF_SIZE 255
 
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
+  while (1) {
+    setbuf(stdout, NULL);
 
-  printf("$ ");
+    printf("$ ");
+
+    char *buf = malloc(BUF_SIZE * sizeof(char));
+
+    fgets(buf, BUF_SIZE, stdin);
+    buf[strcspn(buf, "\n")] = 0;
+
+    printf("%s: command not found\n", buf);
+  }
 
   return 0;
 }
