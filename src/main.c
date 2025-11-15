339589d249be65224931a32307659f843534eb87
nathnael.mekonnen <nathnael.mekonnen@a2sv.org>
2025-11-15 16:21:26 +0100

    feat: add echo and exit support

--- src/main.c
+++ src/main.c
@@ -2,21 +2,80 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUF_SIZE 255
+#define USER_INPUT_BUF_SIZE 256
+#define MAX_ARGS 32
+
+enum COMMAND { EXIT, ECHO, CMD_UNKNOWN };
+
+int parse(char *user_input, char **parsed_command) {
+  int count = 0;
+  char *token = strtok(user_input, " ");
+  while (token != NULL) {
+    parsed_command[count++] = token;
+    token = strtok(NULL, " ");
+  }
+  parsed_command[count] = NULL;
+  return count;
+}
+
+enum COMMAND get_command(char *cmd) {
+  if (cmd == NULL)
+    return CMD_UNKNOWN;
+
+  if (strcmp(cmd, "exit") == 0)
+    return EXIT;
+
+  if (strcmp(cmd, "echo") == 0)
+    return ECHO;
+
+  return CMD_UNKNOWN;
+}
 
 int main(int argc, char *argv[]) {
-  while (1) {
-    setbuf(stdout, NULL);
+  setbuf(stdout, NULL);
 
+  while (1) {
     printf("$ ");
 
-    char *buf = malloc(BUF_SIZE * sizeof(char));
+    char *buf = malloc(USER_INPUT_BUF_SIZE * sizeof(char));
+    char **parsed_command = malloc(MAX_ARGS * sizeof(char *));
 
-    fgets(buf, BUF_SIZE, stdin);
-    buf[strcspn(buf, "\n")] = 0;
+    if (fgets(buf, USER_INPUT_BUF_SIZE, stdin) == NULL) {
+      printf("\n");
+      break;
+    }
 
-    printf("%s: command not found\n", buf);
+    buf[strcspn(buf, "\n")] = '\0';
+
+    int arg_num = parse(buf, parsed_command);
+
+    enum COMMAND cmd = get_command(parsed_command[0]);
+    parsed_command += 1;
+
+    switch (cmd) {
+    case EXIT:
+      if (arg_num > 2) {
+        printf("Too many arguments passed for the command %s, expected %d "
+               "arguments, have %d arguments\n",
+               buf, 2, arg_num);
+      }
+
+      int exit_command_arg;
+      if (sscanf(parsed_command[0], "%d", &exit_command_arg) != 1) {
+        printf("Invalid argument passed, expected number");
+      }
+
+      return exit_command_arg;
+
+    case CMD_UNKNOWN:
+      printf("%s: command not found\n", buf);
+
+    case ECHO:
+      while (*parsed_command != NULL) {
+        printf("%s ", *parsed_command);
+        parsed_command += 1;
+      }
+      printf("\n");
+    }
   }
-
-  return 0;
 }
