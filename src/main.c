1c873f401fc1d8dca5bf8237e026473903844b98
nmktad <nmktad@gmail.com>
2025-12-17 17:50:46 +0100

    refactor: preparing for pipes and rediections

--- src/main.c
+++ src/main.c
@@ -1,16 +1,39 @@
-#include <limits.h> // PATH_MAX (if available)
+#include <errno.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <unistd.h> // access()
+#include <unistd.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
 
 #define USER_INPUT_BUF_SIZE 256
 #define MAX_ARGS 32
 
+typedef struct {
+  int last_status;
+} ShellState;
+
 enum COMMAND { CMD_EXIT, CMD_ECHO, CMD_TYPE, CMD_UNKNOWN, CMD_PWD, CMD_CD };
 
-int run_external(char **argv) {
+static void trim_newline(char *s) {
+  if (!s)
+    return;
+  s[strcspn(s, "\n")] = '\0';
+}
+
+int run_external(char *const argv[], int *out_status_code) {
+  if (out_status_code)
+    *out_status_code = 1;
+
+  if (!argv || !argv[0] || argv[0][0] == '\0') {
+    errno = EINVAL;
+    return -1;
+  }
+
   pid_t pid = fork();
   if (pid < 0) {
     perror("fork");
@@ -19,26 +42,56 @@ int run_external(char **argv) {
 
   if (pid == 0) {
     execvp(argv[0], argv);
-    perror("execvp");
-    _exit(127);
+    int e = errno;
+
+    if (e == ENOENT) {
+      fprintf(stderr, "%s: command not found\n", argv[0]);
+      _exit(127);
+    }
+
+    if (e == EACCES) {
+      fprintf(stderr, "%s: permission denied\n", argv[0]);
+    } else {
+      fprintf(stderr, "%s: %s\n", argv[0], strerror(e));
+    }
+    _exit(126);
   }
 
-  int status;
-  if (waitpid(pid, &status, 0) < 0) {
+  int status = 0;
+  for (;;) {
+    pid_t w = waitpid(pid, &status, 0);
+    if (w == pid)
+      break;
+    if (w < 0 && errno == EINTR)
+      continue;
     perror("waitpid");
     return -1;
   }
 
-  return status;
+  if (!out_status_code)
+    return 0;
+
+  if (WIFEXITED(status)) {
+    *out_status_code = WEXITSTATUS(status);
+  } else if (WIFSIGNALED(status)) {
+    int sig = WTERMSIG(status);
+    *out_status_code = 128 + sig;
+  } else {
+    *out_status_code = 1;
+  }
+
+  return EXIT_SUCCESS;
 }
 
+// TODO: a tokenizer for supporting pipes and redirections
 int parse(char *user_input, char **argv, int max_args) {
   int count = 0;
-  char *token = strtok(user_input, " ");
+  char *saveptr = NULL;
 
-  while (token != NULL && count < max_args - 1) {
+  for (char *token = strtok_r(user_input, " \t", &saveptr);
+       token != NULL && count < max_args - 1;
+       token = strtok_r(NULL, " \t", &saveptr)) {
     argv[count++] = token;
-    token = strtok(NULL, " ");
   }
 
   argv[count] = NULL;
@@ -100,7 +153,9 @@ int find_in_path(const char *name, char *out_buf, size_t out_buf_size) {
 int main(void) {
   setbuf(stdout, NULL);
 
-  while (1) {
+  ShellState st = {.last_status = 0};
+
+  for (;;) {
     printf("$ ");
 
     char buf[USER_INPUT_BUF_SIZE];
@@ -231,16 +286,14 @@ int main(void) {
     }
     case CMD_UNKNOWN:
     default:
-      char path_buf[4096];
-      if (find_in_path(argv[0], path_buf, sizeof(path_buf))) {
-        int status = run_external(argv);
-        if (status == -1) {
-          printf("something went wrong");
-        }
-        break;
+      int code = 0;
+      if (run_external(argv, &code) == -1) {
+        fprintf(stderr, "something went wrong\n");
+        st.last_status = 1;
       } else {
-        printf("%s: command not found\n", command);
+        st.last_status = code;
       }
+      break;
     }
   }
 
